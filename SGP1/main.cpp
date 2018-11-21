51a634a4001db8fc204890a3e4e5b1dfbb0fab48
bt479917 <bt479917@MI104-14>
2018-11-21 17:28:40 +0100

    Initial commit

--- SGP1/main.cpp
+++ SGP1/main.cpp
@@ -24,9 +24,12 @@ void Affichage(list<int> L);
 
 int tubes[4][2];
 int processus[2];
-list<int> Liste = {9,6,3,7,5,4,8};
+list<int> Liste = {9,5,7,5,6,2,1};
 list<int> Liste1;
 list<int> Liste2;
+list<int> EchangeS;
+list<int> EchangeT;
+
 
 int Separator()
 {
@@ -36,11 +39,11 @@ int Separator()
     Liste2= list<int>( middle, Liste.end() );
 }
 
+
 void Affichage(list<int> L)
 {
     for (const auto &l : L ) 
         cout << l << ' ';
-    cout << endl;
 }
 
 int main(int argc, char** argv) {
@@ -55,41 +58,113 @@ int main(int argc, char** argv) {
     if(pid1!=0)
         pid2 = fork();  
     
-    if(pid1==0)
+    if(pid1==0)//S
     {
-        cout<<"S"<<endl;
         int tmp = -INT_MAX;
-        for(const auto &s:Liste1)
-        {
-            if(s>tmp)
+        while(processus[0]==0)
+        {  
+            int pos=0;
+            int posReal=0;
+            for(const auto &s:Liste1)
             {
-                tmp = s;
+                if(s>tmp)
+                {
+                    tmp = s;
+                    pos=posReal;
+                }
+                posReal++;
+            }
+            write(tubes[2][1],&tmp,sizeof(tmp));
+            list<int>::iterator a = next(Liste1.begin(),pos);
+            Liste1.erase(a);
+            EchangeS.push_back(tmp);
+            read(tubes[3][0],&tmp,sizeof(tmp));
+            Liste1.push_back(tmp);
+            cout<<"S:";
+            Affichage(Liste1);
+            cout<<endl;
+            sleep(1);
+            if(EchangeS.size()>3)
+            {
+                
+                list<int>::iterator i = next(EchangeS.begin(),EchangeS.size()-1);
+                list<int>::iterator j = next(EchangeS.begin(),EchangeS.size()-3);
+                if(*i==*j)
+                {
+                    processus[0]=-1;
+                }
             }
         }
-        cout<<tmp<<endl;
-        //write()
-        
+        for (const auto &l : Liste1 )
+        {
+            write(tubes[0][1],&l,sizeof(int));
+        }
+        cout<<"finS"<<endl;
     }
-    else if(pid2==0)
+    else if(pid2==0)//T
     {
-        cout<<"T"<<endl;
+
         int tmp = INT_MAX;
-        for(const auto &t:Liste1)
+        while(processus[1]==0)
         {
-            if(t<tmp)
+            int pos=0;
+            int posReal=0;
+            for(const auto &t:Liste2)
             {
-                tmp = t;
+                if(t<tmp)
+                {
+                    tmp = t;
+                    pos=posReal;
+                }
+                posReal++;
+            }
+            write(tubes[3][1],&tmp,sizeof(tmp));
+            list<int>::iterator a = next(Liste2.begin(),pos);    
+            Liste2.erase(a);
+            EchangeT.push_back(tmp);
+            read(tubes[2][0],&tmp,sizeof(tmp));
+            Liste2.push_back(tmp);
+            cout<<"T:";
+            Affichage(Liste2);
+            cout<<endl;
+            cout<<endl;
+            sleep(1);
+            if(EchangeT.size()>3)
+            {
+                list<int>::iterator i = next(EchangeS.begin(),EchangeS.size()-1);
+                list<int>::iterator j = next(EchangeS.begin(),EchangeS.size()-3);
+                if(*i==*j)
+                {
+                    processus[1]=-1;
+                }
             }
         }
-        cout<<tmp<<endl;
+        for (const auto &l : Liste2 )
+        {
+            write(tubes[1][1],&l,sizeof(int));
+        }
+        cout<<"finT"<<endl;
     }
-     else
+    else//Pere
     {   
         Affichage(Liste1);
         Affichage(Liste2);
-    
+        cout<<endl;
+        cout<<endl;
         int res;
-        read(tubes[0][0],&res,sizeof(res));
+        Liste.clear();
+        for(int i = 0;i<Liste1.size();i++)
+        {
+        read(tubes[0][0],&res,sizeof(int));
+        Liste.push_back(res);
+        }
+        for(int i = 0;i<Liste2.size();i++)
+        {
+        read(tubes[1][0],&res,sizeof(int));
+        Liste.push_back(res);
+        }
+        Affichage(Liste);
+        //read(tubes[1][0],&Liste2,sizeof(Liste2));
     }
     return 0;
 }
